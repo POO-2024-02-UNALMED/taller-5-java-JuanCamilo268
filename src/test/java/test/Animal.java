8c86d40deb9021e60dd9f24dd7fc0a3d92478420
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-08 23:01:27 -0500

    Update Animal.java

--- src/test/java/test/Animal.java
+++ src/test/java/test/Animal.java
@@ -41,8 +41,8 @@ public class Animal {
     public String toString() {
         if (this.zona != null){
             return "Mi nombre es " + nombre + ", tengo una edad de " + edad + ", habito en " + habitat + " y mi genero es " + genero + "la zona en la que me ubico es " + zona + ", en el " + zona.getZoo();
-    }else{
+        }else{
             return "Mi nombre es " + nombre + ", tengo una edad de " + edad + ", habito en " + habitat + " y mi genero es " + genero;
-    }
+        }
     public String movimiento(){return "desplazarse";}
 }
