c4fdb6a733812678fd0872d2794bab1671677f98
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-08 22:18:13 -0500

    Create Zoologico.java

---
+++ src/test/java/test/Zoologico.java
@@ -0,0 +1,40 @@
+package gestion;
+
+public class Zoologico {
+    private String nombre;
+    private String ubicacion;
+    private Zona[] zonas = new Zona[100];
+    public Zoologico (String nombre, String ubicacion){
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+    }
+    public Zoologico (){}
+    public void setNombre(String nombre){this.nombre = nombre;}
+    public void setUbicacion(String ubicacion){this.ubicacion = ubicacion;}
+    public void agregarZonas(Zona zona){
+        for (Zona i:zonas){
+            if (i == null){
+                i = zona;
+                zona.setZoo(this);
+            }
+        }
+    }
+
+    public String getNombre(){return this.nombre;}
+    public String getUbicacion(){return this.ubicacion;}
+    public Zona getZona(){
+        return zonas[0];
+    }
+
+    public int cantidadTotalAnimales() {
+        int total = 0;
+        for (Zona zona : zonas) {
+            if (zona != null) {
+                total += zona.cantidadAnimales();
+            }
+        }
+        return total;
+    }
+
+    public String toString(){return this.nombre;}
+}
